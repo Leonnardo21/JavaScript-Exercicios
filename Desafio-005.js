947c7a87b528aa97304954ec062ce88a0638c752
Leonardo de Souza <leosouza1392@gmail.com>
2020-04-21 17:12:03 -0300

    Exercicio 05

---
+++ Desafio-005.js
@@ -0,0 +1,6 @@
+const formatarValorDecimal = (valorEmReais) =>{
+    valorEmReais = `R$ ${valorEmReais.toFixed(2).toString().replace('.', ',')}`
+    console.log(valorEmReais)
+}
+
+formatarValorDecimal(0.1 + 0.2)
