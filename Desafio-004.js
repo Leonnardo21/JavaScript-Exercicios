3c070dbf8b15a03ca5d3eb7bcb98a4796bf894c9
Leonardo de Souza <leosouza1392@gmail.com>
2020-04-21 17:11:55 -0300

    Exercicio 04

---
+++ Desafio-004.js
@@ -0,0 +1,15 @@
+const division = (dividendo, divisor) => {
+    let result = Math.floor(dividendo / divisor);
+    console.log("Resultado: "+result);
+    console.log("Resto: "+dividendo % divisor);
+  
+  }
+  
+  division(11, 4);
+
+  function divisao (dividendo, divisor) {
+    console.log("Resultado: " + Math.floor(dividendo/divisor));
+    console.log(`Resto: ${dividendo % divisor}`)
+}
+
+divisao(11, 4)
