c38dcfa95e6ae5081eec5ed26f55818ccf37e571
Leonardo de Souza <leosouza1392@gmail.com>
2020-04-21 17:11:22 -0300

    Exercicio 01

---
+++ Desafio-001.js
@@ -0,0 +1,8 @@
+const calc = (n1, n2) => {
+	console.log(`Resultados
+Soma: ${n1} + ${n2} =  ${n1 + n2}
+Subtração: ${n1} - ${n2} =  ${n1 - n2}
+Multiplicação: ${n1} * ${n2} =  ${n1 * n2}
+Divisão: ${n1} / ${n2} =  ${n1 / n2}`)
+}
+calc(2,5)
